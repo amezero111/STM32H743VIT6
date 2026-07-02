a982c393db5c5829d036c55587e5c3ce07d56a42
xyh <3138233928@qq.com>
2026-07-02 21:40:02 +0800

    add MDK ARM project files

---
+++ MDK-ARM/VIT6/arm_kinematics.d
@@ -0,0 +1,5 @@
+vit6/arm_kinematics.o: ..\MOUDLE\arm_kinematics\arm_kinematics.c \
+  ..\MOUDLE\arm_kinematics\arm_kinematics.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\MOUDLE\general_def.h C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\math.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stddef.h
